cfbd623e011710c09e3686b575f3b3e2026ff663
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2025-01-24 11:40:44 -0800

    Setup the CS234 2025 Feedbridge Project (#1)
    # Setup the CS234 2025 Feedbridge Project
    
    ## :gear: Release Notes 
    - Setup the CS234 2025 Feedbridge Project
    
    ## :pencil: Code of Conduct & Contributing Guidelines 
    
    By submitting creating this pull request, you agree to follow our [Code
    of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/CS342/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/CS342/.github/blob/main/CONTRIBUTING.md).

---
+++ FeedbridgeTests/FeedbridgeTests.swift
@@ -0,0 +1,18 @@
+//
+// This source file is part of the Feedbridge based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+@testable import Feedbridge
+import XCTest
+
+
+class FeedbridgeTests: XCTestCase {
+    @MainActor
+    func testContactsCount() throws {
+        XCTAssertEqual(Contacts(presentingAccount: .constant(true)).contacts.count, 1)
+    }
+}
