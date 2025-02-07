6fe415ed7b7408d78c3cb57dd5b4f4386cb4cdb5
Pinlin [Calvin] Xu <calvinxu806@gmail.com>
2025-02-06 17:26:25 -0800

    Updated Baby model for Firebase mapping

--- Feedbridge/Models/Baby.swift
+++ Feedbridge/Models/Baby.swift
@@ -24,19 +24,19 @@ struct Baby: Identifiable, Codable {
     var dateOfBirth: Date
 
     /// Collection of all feeding records
-    var feedEntries: [FeedEntry]
+    var feedEntries: FeedEntries
 
     /// Collection of all weight measurements
-    var weightEntries: [WeightEntry]
+    var weightEntries: WeightEntries
 
     /// Collection of all stool records
-    var stoolEntries: [StoolEntry]
+    var stoolEntries: StoolEntries
 
     /// Collection of all wet diaper records
-    var wetDiaperEntries: [WetDiaperEntry]
+    var wetDiaperEntries: WetDiaperEntries
 
     /// Collection of all dehydration check records
-    var dehydrationChecks: [DehydrationCheck]
+    var dehydrationChecks: DehydrationChecks
 
     /// Calculate baby's age in months (rounded down)
     var ageInMonths: Int {
@@ -45,29 +45,53 @@ struct Baby: Identifiable, Codable {
 
     /// Get the most recent weight entry
     var currentWeight: WeightEntry? {
-        weightEntries.max(by: { $0.dateTime < $1.dateTime })
+        weightEntries.weightEntries.max(by: { $0.dateTime < $1.dateTime })
     }
 
     /// Get the most recent dehydration check
     var latestDehydrationCheck: DehydrationCheck? {
-        dehydrationChecks.max(by: { $0.dateTime < $1.dateTime })
+        dehydrationChecks.dehydrationChecks.max(by: { $0.dateTime < $1.dateTime })
     }
 
     /// Check if there are any active medical alerts
     var hasActiveAlerts: Bool {
         latestDehydrationCheck?.dehydrationAlert == true
-            || wetDiaperEntries.last?.dehydrationAlert == true
-            || stoolEntries.last?.medicalAlert == true
+            || wetDiaperEntries.wetDiaperEntries.last?.dehydrationAlert == true
+            || stoolEntries.stoolEntries.last?.medicalAlert == true
     }
 
-    /// Initialize a new baby with required information
     init(name: String, dateOfBirth: Date) {
         self.name = name
         self.dateOfBirth = dateOfBirth
-        feedEntries = []
-        weightEntries = []
-        stoolEntries = []
-        wetDiaperEntries = []
-        dehydrationChecks = []
+        self.feedEntries = FeedEntries(feedEntries: [])
+        self.weightEntries = WeightEntries(weightEntries: [])
+        self.stoolEntries = StoolEntries(stoolEntries: [])
+        self.wetDiaperEntries = WetDiaperEntries(wetDiaperEntries: [])
+        self.dehydrationChecks = DehydrationChecks(dehydrationChecks: [])
     }
+}
+
+struct FeedEntries: Codable, Identifiable {
+    @DocumentID var id: String?
+    var feedEntries: [FeedEntry]
+}
+
+struct WeightEntries: Codable, Identifiable {
+    @DocumentID var id: String?
+    var weightEntries: [WeightEntry]
+}
+
+struct StoolEntries: Codable, Identifiable {
+    @DocumentID var id: String?
+    var stoolEntries: [StoolEntry]
+}
+
+struct WetDiaperEntries: Codable, Identifiable {
+    @DocumentID var id: String?
+    var wetDiaperEntries: [WetDiaperEntry]
+}
+
+struct DehydrationChecks: Codable, Identifiable {
+    @DocumentID var id: String?
+    var dehydrationChecks: [DehydrationCheck]
 }
